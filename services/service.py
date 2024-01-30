d18138b794bf001a7bed6629a89c37a590d8790e
Andrei Burvel <burvelandrei@gmail.com>
2024-01-30 16:49:22 +0300

    переписана_логика_бота

--- services/service.py
+++ services/service.py
@@ -10,13 +10,13 @@ def _normalize_user_choice(user_choice: str) -> str:
         if LEXICON_RU[key] == user_choice:
             return key
 
-def logic_game(user_choice: str) -> str:
-    bot_choice = get_bot_choice()
+def logic_game(user_choice: str, bot_choice: str) -> str:
+    user_choice = _normalize_user_choice(user_choice)
     win = {'stone': 'scissors',
            'scissors':'paper',
            'paper': 'stone'}
     if win[user_choice] == bot_choice:
-        return LEXICON_RU['user_won']
-    if user_choice == bot_choice:
-        return LEXICON_RU['draw']
-    return LEXICON_RU['bot_won']
+        return 'user_won'
+    elif user_choice == bot_choice:
+        return 'draw'
+    return 'bot_won'
